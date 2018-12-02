866609bac15b3a45f7e477a4479afcf1aaee9489
Energyxxer <energyxxer@gmail.com>
2018-12-02 19:22:03 -0400

    Add list command

---
+++ trident-lang/src/com/energyxxer/trident/compiler/commands/parsers/ListParser.java
@@ -0,0 +1,15 @@
+package com.energyxxer.trident.compiler.commands.parsers;
+
+import com.energyxxer.commodore.functionlogic.commands.Command;
+import com.energyxxer.commodore.functionlogic.commands.list.ListCommand;
+import com.energyxxer.enxlex.pattern_matching.structures.TokenPattern;
+import com.energyxxer.trident.compiler.commands.parsers.general.ParserMember;
+import com.energyxxer.trident.compiler.semantics.TridentFile;
+
+@ParserMember(key = "list")
+public class ListParser implements CommandParser {
+    @Override
+    public Command parse(TokenPattern<?> pattern, TridentFile file) {
+        return new ListCommand();
+    }
+}
