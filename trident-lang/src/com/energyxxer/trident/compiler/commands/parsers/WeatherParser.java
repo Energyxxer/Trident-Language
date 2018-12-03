5aa1b0875d3b61fd9f3d44f7b6c3d90fe15d38ed
Energyxxer <energyxxer@gmail.com>
2018-12-03 00:36:46 -0400

    Add weather command

---
+++ trident-lang/src/com/energyxxer/trident/compiler/commands/parsers/WeatherParser.java
@@ -0,0 +1,18 @@
+package com.energyxxer.trident.compiler.commands.parsers;
+
+import com.energyxxer.commodore.functionlogic.commands.Command;
+import com.energyxxer.commodore.functionlogic.commands.weather.WeatherCommand;
+import com.energyxxer.enxlex.pattern_matching.structures.TokenPattern;
+import com.energyxxer.trident.compiler.commands.parsers.general.ParserMember;
+import com.energyxxer.trident.compiler.semantics.TridentFile;
+
+@ParserMember(key = "weather")
+public class WeatherParser implements CommandParser {
+    @Override
+    public Command parse(TokenPattern<?> pattern, TridentFile file) {
+        WeatherCommand.Mode mode = WeatherCommand.Mode.valueOf(pattern.find("CHOICE").flatten(false).toUpperCase());
+        TokenPattern<?> rawDuration = pattern.find("INTEGER");
+        return (rawDuration != null) ? new WeatherCommand(mode, Integer.parseInt(rawDuration.flatten(false))) :
+                new WeatherCommand(mode);
+    }
+}
