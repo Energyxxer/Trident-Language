99ddb28d1210fb3e1d0be09991c222cceb9074f8
Energyxxer <energyxxer@gmail.com>
2018-12-03 18:06:54 -0400

    Add x_rotation selector argument

---
+++ trident-lang/src/com/energyxxer/trident/compiler/commands/parsers/constructs/selectors/YawArgumentParser.java
@@ -0,0 +1,16 @@
+package com.energyxxer.trident.compiler.commands.parsers.constructs.selectors;
+
+import com.energyxxer.commodore.functionlogic.selector.arguments.SelectorArgument;
+import com.energyxxer.commodore.functionlogic.selector.arguments.YawArgument;
+import com.energyxxer.enxlex.pattern_matching.structures.TokenPattern;
+import com.energyxxer.trident.compiler.TridentCompiler;
+import com.energyxxer.trident.compiler.commands.parsers.constructs.CommonParsers;
+import com.energyxxer.trident.compiler.commands.parsers.general.ParserMember;
+
+@ParserMember(key = "x_rotation")
+public class YawArgumentParser implements SelectorArgumentParser {
+    @Override
+    public SelectorArgument parse(TokenPattern<?> pattern, TridentCompiler compiler) {
+        return new YawArgument(CommonParsers.parseRealRange(pattern));
+    }
+}
