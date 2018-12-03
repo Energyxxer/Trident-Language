fcabd77dd86232f84b828a03cda08bb91051cd20
Energyxxer <energyxxer@gmail.com>
2018-12-03 18:09:58 -0400

    Add level selector argument

---
+++ trident-lang/src/com/energyxxer/trident/compiler/commands/parsers/constructs/selectors/LevelArgumentParser.java
@@ -0,0 +1,16 @@
+package com.energyxxer.trident.compiler.commands.parsers.constructs.selectors;
+
+import com.energyxxer.commodore.functionlogic.selector.arguments.LevelArgument;
+import com.energyxxer.commodore.functionlogic.selector.arguments.SelectorArgument;
+import com.energyxxer.enxlex.pattern_matching.structures.TokenPattern;
+import com.energyxxer.trident.compiler.TridentCompiler;
+import com.energyxxer.trident.compiler.commands.parsers.constructs.CommonParsers;
+import com.energyxxer.trident.compiler.commands.parsers.general.ParserMember;
+
+@ParserMember(key = "level")
+public class LevelArgumentParser implements SelectorArgumentParser {
+    @Override
+    public SelectorArgument parse(TokenPattern<?> pattern, TridentCompiler compiler) {
+        return new LevelArgument(CommonParsers.parseIntRange(pattern));
+    }
+}
