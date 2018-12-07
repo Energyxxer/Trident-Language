4ef0c83b9922d446af972de1635cd1670f591b63
Energyxxer <energyxxer@gmail.com>
2018-12-07 17:59:54 -0400

    Fix give and clear using a fixed selector

--- trident-lang/src/com/energyxxer/trident/compiler/commands/parsers/commands/ClearParser.java
+++ trident-lang/src/com/energyxxer/trident/compiler/commands/parsers/commands/ClearParser.java
@@ -2,11 +2,10 @@ package com.energyxxer.trident.compiler.commands.parsers.commands;
 
 import com.energyxxer.commodore.functionlogic.commands.Command;
 import com.energyxxer.commodore.functionlogic.commands.clear.ClearCommand;
-import com.energyxxer.commodore.functionlogic.entity.GenericEntity;
-import com.energyxxer.commodore.functionlogic.selector.Selector;
 import com.energyxxer.commodore.item.Item;
 import com.energyxxer.enxlex.pattern_matching.structures.TokenPattern;
 import com.energyxxer.trident.compiler.commands.parsers.constructs.CommonParsers;
+import com.energyxxer.trident.compiler.commands.parsers.constructs.EntityParser;
 import com.energyxxer.trident.compiler.commands.parsers.general.ParserMember;
 import com.energyxxer.trident.compiler.semantics.TridentFile;
 
@@ -17,6 +16,6 @@ public class ClearParser implements CommandParser {
         Item item = CommonParsers.parseItem(pattern.find("..ITEM_TAGGED"), file.getCompiler());
         TokenPattern<?> amountPattern = pattern.find("..AMOUNT");
         int amount = amountPattern != null ? Integer.parseInt(amountPattern.flattenTokens().get(0).value) : -1;
-        return new ClearCommand(new GenericEntity(new Selector(Selector.BaseSelector.SENDER)), item, amount);
+        return new ClearCommand(EntityParser.parseEntity(pattern.find(".ENTITY"), file.getCompiler()), item, amount);
     }
 }
--- trident-lang/src/com/energyxxer/trident/compiler/commands/parsers/commands/GiveParser.java
+++ trident-lang/src/com/energyxxer/trident/compiler/commands/parsers/commands/GiveParser.java
@@ -2,11 +2,10 @@ package com.energyxxer.trident.compiler.commands.parsers.commands;
 
 import com.energyxxer.commodore.functionlogic.commands.Command;
 import com.energyxxer.commodore.functionlogic.commands.give.GiveCommand;
-import com.energyxxer.commodore.functionlogic.entity.GenericEntity;
-import com.energyxxer.commodore.functionlogic.selector.Selector;
 import com.energyxxer.commodore.item.Item;
 import com.energyxxer.enxlex.pattern_matching.structures.TokenPattern;
 import com.energyxxer.trident.compiler.commands.parsers.constructs.CommonParsers;
+import com.energyxxer.trident.compiler.commands.parsers.constructs.EntityParser;
 import com.energyxxer.trident.compiler.commands.parsers.general.ParserMember;
 import com.energyxxer.trident.compiler.semantics.TridentFile;
 
@@ -17,6 +16,7 @@ public class GiveParser implements CommandParser {
         Item item = CommonParsers.parseItem(pattern.find("ITEM"), file.getCompiler());
         TokenPattern<?> amountPattern = pattern.find("AMOUNT");
         int amount = amountPattern != null ? Integer.parseInt(amountPattern.flattenTokens().get(0).value) : 1;
-        return new GiveCommand(new GenericEntity(new Selector(Selector.BaseSelector.SENDER)), item, amount);
+
+        return new GiveCommand(EntityParser.parseEntity(pattern.find("ENTITY"), file.getCompiler()), item, amount);
     }
 }
