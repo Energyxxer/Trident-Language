12056efca8bbc4c5bd1afecbb2798e8ccce2268f
Energyxxer <energyxxer@gmail.com>
2019-02-07 13:15:36 -0400

    Add picked up event for custom items

--- trident-lang/src/com/energyxxer/trident/compiler/semantics/custom/special/item_events/criteria/PickedUpScoreEvent.java
+++ trident-lang/src/com/energyxxer/trident/compiler/semantics/custom/special/item_events/criteria/PickedUpScoreEvent.java
@@ -1,13 +1,18 @@
 package com.energyxxer.trident.compiler.semantics.custom.special.item_events.criteria;
 
-import com.energyxxer.commodore.functionlogic.commands.execute.ExecuteCommand;
-import com.energyxxer.commodore.functionlogic.commands.execute.ExecuteConditionEntity;
-import com.energyxxer.commodore.functionlogic.commands.execute.ExecuteModifier;
+import com.energyxxer.commodore.functionlogic.commands.clear.ClearCommand;
+import com.energyxxer.commodore.functionlogic.commands.execute.*;
 import com.energyxxer.commodore.functionlogic.commands.function.FunctionCommand;
+import com.energyxxer.commodore.functionlogic.commands.scoreboard.ScorePlayersOperation;
+import com.energyxxer.commodore.functionlogic.score.LocalScore;
+import com.energyxxer.commodore.functionlogic.score.Objective;
+import com.energyxxer.commodore.functionlogic.score.PlayerName;
 import com.energyxxer.commodore.functionlogic.selector.Selector;
 import com.energyxxer.commodore.functionlogic.selector.arguments.ScoreArgument;
+import com.energyxxer.commodore.textcomponents.StringTextComponent;
 import com.energyxxer.commodore.util.NumberRange;
 import com.energyxxer.trident.compiler.analyzers.general.AnalyzerMember;
+import com.energyxxer.trident.compiler.semantics.custom.items.NBTMode;
 import com.energyxxer.trident.compiler.semantics.custom.special.SpecialFileManager;
 import com.energyxxer.trident.compiler.semantics.custom.special.item_events.ItemEvent;
 import com.energyxxer.trident.compiler.semantics.custom.special.item_events.ItemEventFile;
@@ -39,11 +44,26 @@ public class PickedUpScoreEvent implements ScoreEventCriteriaHandler {
         modifiers.add(new ExecuteConditionEntity(IF, initialSelector));
 
         if(data.customItem != null) {
+            Objective objective = data.compiler.getModule().getObjectiveManager().create("ptitem." + data.customItem.getItemIdHash(), "dummy", new StringTextComponent("Temporary picked_up " + data.customItem), true);
 
-        }
+            //store result score #OPERATION ptitem.### clear @s <citem> 0
+            data.function.append(new ExecuteCommand(new ClearCommand(new Selector(Selector.BaseSelector.SENDER),data.customItem.constructItem(NBTMode.TESTING), 0), new ExecuteStoreScore(new LocalScore(new PlayerName("#OPERATION"), objective))));
 
-        for(ItemEvent event : data.events) {
-            data.function.append(new ExecuteCommand(new FunctionCommand(event.toCall), modifiers));
+            //scoreboard players operation #OPERATION ptitem.### -= @s ptitem.###
+            data.function.append(new ScorePlayersOperation(new LocalScore(new PlayerName("#OPERATION"), objective), ScorePlayersOperation.Operation.SUBTRACT, new LocalScore(new Selector(Selector.BaseSelector.SENDER), objective)));
+
+            modifiers.add(new ExecuteConditionScoreMatch(IF, new LocalScore(new PlayerName("#OPERATION"), objective), new NumberRange<>(1, null)));
+
+            for(ItemEvent event : data.events) {
+                data.function.append(new ExecuteCommand(new FunctionCommand(event.toCall), modifiers));
+            }
+
+            data.function.append(new ExecuteCommand(new ClearCommand(new Selector(Selector.BaseSelector.SENDER),data.customItem.constructItem(NBTMode.TESTING), 0), new ExecuteStoreScore(new LocalScore(new Selector(Selector.BaseSelector.SENDER), objective))));
+
+        } else {
+            for(ItemEvent event : data.events) {
+                data.function.append(new ExecuteCommand(new FunctionCommand(event.toCall), modifiers));
+            }
         }
     }
 
