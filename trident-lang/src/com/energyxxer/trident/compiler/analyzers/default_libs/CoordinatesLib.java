77bcbcb60114f24479ab54b69535b0485aa01828
Energyxxer <energyxxer@gmail.com>
2019-03-17 17:28:03 -0400

    Add methods to the coordinates type

---
+++ trident-lang/src/com/energyxxer/trident/compiler/analyzers/default_libs/CoordinatesLib.java
@@ -0,0 +1,54 @@
+package com.energyxxer.trident.compiler.analyzers.default_libs;
+
+import com.energyxxer.commodore.functionlogic.coordinates.Coordinate;
+import com.energyxxer.trident.compiler.TridentCompiler;
+import com.energyxxer.trident.compiler.analyzers.general.AnalyzerMember;
+import com.energyxxer.trident.compiler.analyzers.type_handlers.DictionaryObject;
+import com.energyxxer.trident.compiler.semantics.Symbol;
+import com.energyxxer.trident.compiler.semantics.symbols.ISymbolContext;
+
+@AnalyzerMember(key = "Coordinates")
+public class CoordinatesLib implements DefaultLibraryProvider {
+
+    public static final String COORDINATE_TYPE_ABSOLUTE =   "Coordinates.ABSOLUTE";
+    public static final String COORDINATE_TYPE_RELATIVE =   "Coordinates.RELATIVE";
+    public static final String COORDINATE_TYPE_LOCAL =      "Coordinates.LOCAL";
+
+    public static final String AXIS_X = "Axis.X";
+    public static final String AXIS_Y = "Axis.Y";
+    public static final String AXIS_Z = "Axis.Z";
+
+
+    @Override
+    public void populate(ISymbolContext globalCtx, TridentCompiler compiler) {
+        DictionaryObject coord = new DictionaryObject();
+
+        coord.put("ABSOLUTE", COORDINATE_TYPE_ABSOLUTE);
+        coord.put("RELATIVE", COORDINATE_TYPE_RELATIVE);
+        coord.put("LOCAL",    COORDINATE_TYPE_LOCAL);
+
+        globalCtx.put(new Symbol("Coordinates", Symbol.SymbolVisibility.GLOBAL, coord));
+
+
+        DictionaryObject axis = new DictionaryObject();
+
+        axis.put("X", AXIS_X);
+        axis.put("Y", AXIS_Y);
+        axis.put("Z", AXIS_Z);
+
+        globalCtx.put(new Symbol("Axis", Symbol.SymbolVisibility.GLOBAL, axis));
+    }
+
+    public static String coordTypeToConstant(Coordinate.Type type) {
+        return type == Coordinate.Type.RELATIVE ? COORDINATE_TYPE_RELATIVE : type == Coordinate.Type.LOCAL ? COORDINATE_TYPE_LOCAL : COORDINATE_TYPE_ABSOLUTE;
+    }
+
+    public static Coordinate.Type constantToCoordType(String cons) {
+        switch(cons) {
+            case COORDINATE_TYPE_ABSOLUTE: return Coordinate.Type.ABSOLUTE;
+            case COORDINATE_TYPE_RELATIVE: return Coordinate.Type.RELATIVE;
+            case COORDINATE_TYPE_LOCAL: return Coordinate.Type.LOCAL;
+            default: throw new IllegalArgumentException("Invalid axis argument '" + cons + "'. Use constants Coordinates.ABSOLUTE, Coordinates.RELATIVE and Coordinates.LOCAL to specify a coordinate type.");
+        }
+    }
+}
---
+++ trident-lang/src/com/energyxxer/trident/compiler/analyzers/type_handlers/extensions/CoordinateTypeHandler.java
@@ -0,0 +1,119 @@
+package com.energyxxer.trident.compiler.analyzers.type_handlers.extensions;
+
+import com.energyxxer.commodore.functionlogic.coordinates.Coordinate;
+import com.energyxxer.commodore.functionlogic.coordinates.CoordinateSet;
+import com.energyxxer.enxlex.pattern_matching.structures.TokenPattern;
+import com.energyxxer.trident.compiler.analyzers.default_libs.CoordinatesLib;
+import com.energyxxer.trident.compiler.analyzers.general.AnalyzerMember;
+import com.energyxxer.trident.compiler.analyzers.type_handlers.MemberNotFoundException;
+import com.energyxxer.trident.compiler.analyzers.type_handlers.MemberWrapper;
+import com.energyxxer.trident.compiler.analyzers.type_handlers.MethodWrapper;
+import com.energyxxer.trident.compiler.analyzers.type_handlers.VariableTypeHandler;
+import com.energyxxer.trident.compiler.semantics.symbols.ISymbolContext;
+
+import java.util.HashMap;
+
+@AnalyzerMember(key = "com.energyxxer.commodore.functionlogic.coordinates.CoordinateSet")
+public class CoordinateTypeHandler implements VariableTypeHandler<CoordinateSet> {
+    private static HashMap<String, MemberWrapper<CoordinateSet>> members = new HashMap<>();
+
+    static {
+        members.put("getMagnitude", new MethodWrapper<>("getMagnitude", (instance, params) -> {
+            switch((String)params[0]) {
+                case CoordinatesLib.AXIS_X: {
+                    return instance.getX().getCoord();
+                }
+                case CoordinatesLib.AXIS_Y: {
+                    return instance.getY().getCoord();
+                }
+                case CoordinatesLib.AXIS_Z: {
+                    return instance.getZ().getCoord();
+                }
+                default: {
+                    throw new IllegalArgumentException("Invalid axis argument '" + params[0] + "'. Use constants Axis.X, Axis.Y and Axis.Z to specify an axis.");
+                }
+            }
+        }, String.class));
+        members.put("getCoordinateType", new MethodWrapper<>("getCoordinateType", (instance, params) -> {
+            switch((String)params[0]) {
+                case CoordinatesLib.AXIS_X: {
+                    return CoordinatesLib.coordTypeToConstant(instance.getX().getType());
+                }
+                case CoordinatesLib.AXIS_Y: {
+                    return CoordinatesLib.coordTypeToConstant(instance.getY().getType());
+                }
+                case CoordinatesLib.AXIS_Z: {
+                    return CoordinatesLib.coordTypeToConstant(instance.getZ().getType());
+                }
+                default: {
+                    throw new IllegalArgumentException("Invalid axis argument '" + params[0] + "'. Use constants Axis.X, Axis.Y and Axis.Z to specify an axis.");
+                }
+            }
+        }, String.class));
+        members.put("deriveMagnitude", new MethodWrapper<CoordinateSet>("deriveMagnitude", (instance, params) -> {
+            double magnitude = ((double) params[0]);
+            if(params[1] == null) {
+                return new CoordinateSet(
+                        new Coordinate(instance.getX().getType(), magnitude),
+                        new Coordinate(instance.getY().getType(), magnitude),
+                        new Coordinate(instance.getZ().getType(), magnitude)
+                );
+            }
+            switch((String)params[1]) {
+                case CoordinatesLib.AXIS_X: {
+                    return new CoordinateSet(new Coordinate(instance.getX().getType(), magnitude), instance.getY(), instance.getZ());
+                }
+                case CoordinatesLib.AXIS_Y: {
+                    return new CoordinateSet(instance.getX(), new Coordinate(instance.getY().getType(), magnitude), instance.getZ());
+                }
+                case CoordinatesLib.AXIS_Z: {
+                    return new CoordinateSet(instance.getX(), instance.getY(), new Coordinate(instance.getZ().getType(), magnitude));
+                }
+                default: {
+                    throw new IllegalArgumentException("Invalid axis argument '" + params[0] + "'. Use constants Axis.X, Axis.Y and Axis.Z to specify an axis.");
+                }
+            }
+        }, Double.class, String.class).setNullable(1));
+        members.put("deriveCoordinateType", new MethodWrapper<CoordinateSet>("deriveCoordinateType", (instance, params) -> {
+            Coordinate.Type type = CoordinatesLib.constantToCoordType((String) params[0]);
+            if(params[1] == null || type == Coordinate.Type.LOCAL || instance.getX().getType() == Coordinate.Type.LOCAL) {
+                return new CoordinateSet(
+                        new Coordinate(type, instance.getX().getCoord()),
+                        new Coordinate(type, instance.getY().getCoord()),
+                        new Coordinate(type, instance.getZ().getCoord())
+                );
+            }
+            switch((String)params[1]) {
+                case CoordinatesLib.AXIS_X: {
+                    return new CoordinateSet(new Coordinate(type, instance.getX().getCoord()), instance.getY(), instance.getZ());
+                }
+                case CoordinatesLib.AXIS_Y: {
+                    return new CoordinateSet(instance.getX(), new Coordinate(type, instance.getY().getCoord()), instance.getZ());
+                }
+                case CoordinatesLib.AXIS_Z: {
+                    return new CoordinateSet(instance.getX(), instance.getY(), new Coordinate(type, instance.getZ().getCoord()));
+                }
+                default: {
+                    throw new IllegalArgumentException("Invalid axis argument '" + params[0] + "'. Use constants Axis.X, Axis.Y and Axis.Z to specify an axis.");
+                }
+            }
+        }, String.class, String.class).setNullable(1));
+    }
+
+    @Override
+    public Object getMember(CoordinateSet coords, String member, TokenPattern<?> pattern, ISymbolContext ctx, boolean keepSymbol) {
+        MemberWrapper<CoordinateSet> result = members.get(member);
+        if(result == null) throw new MemberNotFoundException();
+        return result.unwrap(coords);
+    }
+
+    @Override
+    public Object getIndexer(CoordinateSet object, Object index, TokenPattern<?> pattern, ISymbolContext ctx, boolean keepSymbol) {
+        throw new MemberNotFoundException();
+    }
+
+    @Override
+    public <F> F cast(CoordinateSet object, Class<F> targetType, TokenPattern<?> pattern, ISymbolContext ctx) {
+        throw new ClassCastException();
+    }
+}
