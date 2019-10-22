1ee0f86e5f01a90aeb1061b148a0c9c72f04b8ae
Energyxxer <energyxxer@gmail.com>
2019-10-22 16:25:33 -0400

    Fix "rotated as" outputting "positioned as"

--- trident-lang/src/com/energyxxer/trident/compiler/analyzers/modifiers/RotatedParser.java
+++ trident-lang/src/com/energyxxer/trident/compiler/analyzers/modifiers/RotatedParser.java
@@ -1,14 +1,14 @@
 package com.energyxxer.trident.compiler.analyzers.modifiers;
 
 import com.energyxxer.commodore.functionlogic.commands.execute.ExecuteModifier;
-import com.energyxxer.commodore.functionlogic.commands.execute.ExecutePositionedAsEntity;
+import com.energyxxer.commodore.functionlogic.commands.execute.ExecuteRotatedAsEntity;
 import com.energyxxer.enxlex.pattern_matching.structures.TokenPattern;
 import com.energyxxer.enxlex.pattern_matching.structures.TokenStructure;
 import com.energyxxer.trident.compiler.analyzers.constructs.CoordinateParser;
 import com.energyxxer.trident.compiler.analyzers.constructs.EntityParser;
 import com.energyxxer.trident.compiler.analyzers.general.AnalyzerMember;
-import com.energyxxer.trident.compiler.semantics.symbols.ISymbolContext;
 import com.energyxxer.trident.compiler.semantics.TridentException;
+import com.energyxxer.trident.compiler.semantics.symbols.ISymbolContext;
 
 @AnalyzerMember(key = "rotated")
 public class RotatedParser implements SimpleModifierParser {
@@ -17,7 +17,7 @@ public class RotatedParser implements SimpleModifierParser {
         TokenPattern<?> branch = ((TokenStructure) pattern.find("CHOICE")).getContents();
         switch(branch.getName()) {
             case "ENTITY_BRANCH": {
-                return new ExecutePositionedAsEntity(EntityParser.parseEntity(branch.find("ENTITY"), ctx));
+                return new ExecuteRotatedAsEntity(EntityParser.parseEntity(branch.find("ENTITY"), ctx));
             }
             case "TWO_COORDINATE_SET": {
                 return CoordinateParser.parseRotation(branch, ctx);
