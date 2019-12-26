48125f97befee06e529f85a23107470440dd5c72
Energyxxer <energyxxer@gmail.com>
2019-12-26 16:09:23 -0400

    Fix score text components quoting selectors

--- trident-lang/src/com/energyxxer/trident/compiler/analyzers/constructs/TextParser.java
+++ trident-lang/src/com/energyxxer/trident/compiler/analyzers/constructs/TextParser.java
@@ -335,8 +335,10 @@ public class TextParser {
     }
 
     static class RawEntity extends PlayerName {
+        private String rawName;
         public RawEntity(@NotNull String name) {
             super(name);
+            this.rawName = name;
         }
 
         @Override
@@ -377,6 +379,11 @@ public class TextParser {
         @Override
         public void assertAvailable() {
 
+        }
+
+        @Override
+        public @NotNull String scoreHolderToString() {
+            return rawName;
         }
     }
 }
