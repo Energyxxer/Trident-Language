6f75d21f1ae214d9a8232a872774862ca333482f
Energyxxer <energyxxer@gmail.com>
2020-01-14 20:38:13 -0400

    Increment version number

--- trident-lang/src/com/energyxxer/trident/compiler/TridentCompiler.java
+++ trident-lang/src/com/energyxxer/trident/compiler/TridentCompiler.java
@@ -58,7 +58,7 @@ public class TridentCompiler extends AbstractProcess {
 
     public static final String PROJECT_FILE_NAME = ".tdnproj";
     public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
-    public static final String TRIDENT_LANGUAGE_VERSION = "0.5.1-beta";
+    public static final String TRIDENT_LANGUAGE_VERSION = "0.5.2-beta";
 
     //Resources
     private DefinitionPack[] defaultDefinitionPacks;
