f44b6370fe8cc2f49ae905a142e135a5058919d7
GeneryDev <generydev@gmail.com>
2020-08-16 13:07:58 -0400

    Increment version number

--- trident-lang/src/com/energyxxer/trident/compiler/TridentCompiler.java
+++ trident-lang/src/com/energyxxer/trident/compiler/TridentCompiler.java
@@ -52,7 +52,7 @@ public class TridentCompiler extends AbstractProcess {
     public static final String PROJECT_FILE_NAME = ".tdnproj";
     public static final String PROJECT_BUILD_FILE_NAME = ".tdnbuild";
     public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
-    public static final String TRIDENT_LANGUAGE_VERSION = "1.2.0";
+    public static final String TRIDENT_LANGUAGE_VERSION = "1.2.1";
 
     //Resources
     private TridentProjectWorker worker;
