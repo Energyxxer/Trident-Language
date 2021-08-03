778cee90158ee843a462f3c0478c7b3d1809f6f6
Energyxxer <energyxxer@gmail.com>
2021-08-03 11:06:22 -0400

    Fix dictionary keys not unescaping single-quote strings

--- trident-lang/src/com/energyxxer/trident/sets/DataStructureLiteralSet.java
+++ trident-lang/src/com/energyxxer/trident/sets/DataStructureLiteralSet.java
@@ -42,7 +42,7 @@ public class DataStructureLiteralSet extends PatternProviderSet { //dictionaries
                                         .addProcessor((p, l) -> {
                                             if(l.getSummaryModule() != null) {
                                                 String key = p.flatten(false);
-                                                if(key.startsWith("\"")) {
+                                                if(key.startsWith("\"") || key.startsWith("'")) {
                                                     try {
                                                         key = CommandUtils.parseQuotedString(key);
                                                     } catch(CommodoreException ignore) {
